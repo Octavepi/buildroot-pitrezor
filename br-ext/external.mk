cd0dcba67cf32d9e2699b8acc8633ff6bdde35de
Octavepi <101262679+Octavepi@users.noreply.github.com>
2025-10-05 00:05:33 -0700

    more fixes

--- br-ext/external.mk
+++ br-ext/external.mk
@@ -1,1 +1,3 @@
-# Placeholder for global package logic if needed.
+# External makefile for PiTrezor
+# Currently no global package logic is required.
+# This file exists so Buildroot can recognize br-ext/ as an external tree.
