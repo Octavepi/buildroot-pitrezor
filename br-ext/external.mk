608054f1e668d9a0a0defa784ef4e6cbc967065d
Octavepi <101262679+Octavepi@users.noreply.github.com>
2025-10-05 22:10:09 -0700

    more fixes

--- br-ext/external.mk
+++ br-ext/external.mk
@@ -1,3 +1,8 @@
 # External.mk for PiTrezor br-ext
 # Auto-include all package makefiles in br-ext/package
 include $(sort $(wildcard $(BR2_EXTERNAL_PITREZOR_PATH)/package/*/*.mk))
+
+# Enforce PIE, RELRO, and hardening flags on all packages
+TARGET_CFLAGS += -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -pie
+TARGET_LDFLAGS += -Wl,-z,relro,-z,now
+
