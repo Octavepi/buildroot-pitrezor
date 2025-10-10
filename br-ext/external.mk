cb5dc0f79e11e70757c8bfbbafb599ec9f23ec69
Octavepi <101262679+Octavepi@users.noreply.github.com>
2025-10-10 13:12:45 -0700

    af

--- br-ext/external.mk
+++ br-ext/external.mk
@@ -2,6 +2,8 @@
 # Auto-include all package makefiles in br-ext/package
 include $(sort $(wildcard $(BR2_EXTERNAL_PITREZOR_PATH)/package/*/*.mk))
 
-# Enforce PIE, RELRO, and hardening flags on all packages
-TARGET_CFLAGS += -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE
-TARGET_LDFLAGS += -Wl,-z,relro,-z,now -pie
+# Note: Hardening flags are controlled via Buildroot config fragments
+# (see br-ext/hardening/*.fragment). Avoid injecting global TARGET_* flags here
+# to prevent leaking -pie/-fPIE into toolchain builds (e.g., libgcc), which can
+# cause link failures. Target packages will inherit the appropriate flags from
+# the selected BR2_* options.
